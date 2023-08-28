bc8fdde9359f2d98368a711adbfcc830c6b72cec
ji <6531z@naver.com>
2023-08-29 01:27:05 +0900

    user-service

--- 클라우드 개발/user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ 클라우드 개발/user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -2,20 +2,24 @@ package com.example.userservice.controller;
 
 import com.example.userservice.vo.Greeting;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/user-service")
 @RequiredArgsConstructor
 public class UserController {
 
     private final Greeting greeting;
+    private final Environment env;
 
     @GetMapping("/health_check")
     public String status(){
-        return "It's Working in User Service";
+
+        return  String.format( "It's Working in User Service on PORT %s",
+                env.getProperty("local.server.port"));
     }
 
     @GetMapping("/welcome")
