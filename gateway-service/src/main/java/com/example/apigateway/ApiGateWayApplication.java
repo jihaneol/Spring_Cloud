325860f785904ab823aab56902b44a82d2b78878
6531z <6531z@naver.com>
2023-09-05 15:49:09 +0900

    수정

--- gateway-service/src/main/java/com/example/apigateway/ApiGateWayApplication.java
+++ gateway-service/src/main/java/com/example/apigateway/ApiGateWayApplication.java
@@ -1,14 +1,23 @@
 package com.example.apigateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiGateWayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGateWayApplication.class, args);
+	}
+
+
+	@Bean
+	public HttpTraceRepository httpTraceRepository() {
+		return new InMemoryHttpTraceRepository();
 	}
 
 }
